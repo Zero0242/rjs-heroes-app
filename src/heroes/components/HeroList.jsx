5807813053754422a1156ca7ea63727bf8ffc08c
Zero0242 <91895271+Zero0242@users.noreply.github.com>
2023-12-20 21:55:19 -0300

    HeroCard

--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -1,13 +1,14 @@
+import { HeroCard } from "./";
 import { getHeroesByPublisher } from "../helpers";
 
 export const HeroList = ({ publisher }) => {
   const heroList = getHeroesByPublisher(publisher);
 
   return (
-    <ul>
+    <div className='row rows-cols-1 row-cols-md-3 g-3'>
       {heroList.map((hero) => (
-        <li key={hero}>{hero.superhero}</li>
+        <HeroCard key={hero.id} {...hero} />
       ))}
-    </ul>
+    </div>
   );
 };
