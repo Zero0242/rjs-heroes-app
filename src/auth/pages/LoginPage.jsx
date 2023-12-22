e6a30d4ea150144e09c3474baed05acf36200f8d
Zero0242 <91895271+Zero0242@users.noreply.github.com>
2023-12-22 01:09:43 -0300

    usar ultima ruta

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -7,8 +7,9 @@ export const LoginPage = () => {
   const { userLogin } = useContext(AuthContext);
 
   const onLogin = () => {
+    const lastPath = localStorage.getItem("lastPath") ?? "/";
     userLogin("Pepino");
-    navigate("/", { replace: true });
+    navigate(lastPath, { replace: true });
   };
 
   return (
