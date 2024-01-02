5aef95a23bfaeba3f723530695d4e2b6772c3121
Zero0242 <91895271+Zero0242@users.noreply.github.com>
2024-01-02 14:21:14 -0300

    Terminando TEST. Search PAGE

--- tests/heroes/pages/SearchPage.test.jsx
+++ tests/heroes/pages/SearchPage.test.jsx
@@ -1,8 +1,17 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import { SearchPage } from "../../../src/heroes/pages/";
 
+const mockedUseNavigate = jest.fn();
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockedUseNavigate,
+}));
+
 describe("Pruebas en <SearchPage />", () => {
+  beforeEach(() => jest.clearAllMocks());
+
   test("debe de mostar correctamente al estar con los valores por defecto", () => {
     const { container } = render(
       <MemoryRouter>
@@ -32,5 +41,44 @@ describe("Pruebas en <SearchPage />", () => {
     const alertDiv = screen.getByLabelText("alert-danger");
 
     expect(alertDiv.style.display).toBe("none");
+  });
+
+  test("debe de mostrar un error si no se encuentra el hero", () => {
+    const heroe = "HomeLander";
+    render(
+      <MemoryRouter initialEntries={[`/search?q=${heroe}`]}>
+        <SearchPage />
+      </MemoryRouter>
+    );
+
+    // screen.debug();
+    const alertDiv = screen.getByLabelText("alert-danger");
+
+    expect(alertDiv.style.display).not.toBe("none");
+  });
+
+  test("debe de llamar el navigate a la pantalla nueva", () => {
+    render(
+      <MemoryRouter initialEntries={[`/search}`]}>
+        <SearchPage />
+      </MemoryRouter>
+    );
+
+    const input = screen.getByRole("textbox");
+
+    fireEvent.change(input, {
+      target: {
+        value: "superman",
+        name: "searchQuery",
+      },
+    });
+
+    const form = screen.getByRole("form");
+
+    fireEvent.submit(form);
+
+
+
+    expect(mockedUseNavigate).toHaveBeenCalledWith("?q=superman")
   });
 });
